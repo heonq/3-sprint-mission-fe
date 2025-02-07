874cb8d4e1dd10c7c08760dd38dfecf69393649f
heonq <gkagjsrb@naver.com>
2025-02-07 14:36:39 +0900

    fix: getArticleComments api 함수 파라미터 수정

--- src/services/api/article.ts
+++ src/services/api/article.ts
@@ -93,7 +93,7 @@ export const getArticleComments = async (
       `/articles/${articleId}/comments`,
       {
         params: {
-          limit,
+          take: limit,
         },
       },
     );
