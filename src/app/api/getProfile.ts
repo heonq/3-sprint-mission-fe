a5c82ce4064dc37458f854bd0a1eb865797e9f6e
heonq <gkagjsrb@naver.com>
2025-02-10 18:51:27 +0900

    test

--- src/app/api/getProfile.ts
+++ src/app/api/getProfile.ts
@@ -17,7 +17,7 @@ export default async function handler(
 
     const data = await response.json();
     return res.status(200).json(data);
-  } catch (error) {
+  } catch {
     return res.status(500).json({ error: 'Failed to fetch data' });
   }
 }
