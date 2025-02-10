1c153adeece77dd7ca03399912b066419fc072b5
heonq <gkagjsrb@naver.com>
2025-02-10 15:18:16 +0900

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { jwtVerify } from 'jose';
-import { cookies } from 'next/headers';
+// import { cookies } from 'next/headers';
 // import { JWTExpired } from 'jose/errors';
 // import { cookies } from 'next/headers';
 
