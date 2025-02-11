c0c23f3bba8ae10105dc0e191741afaa11c0dcd9
heonq <gkagjsrb@naver.com>
2025-02-11 09:49:18 +0900

    fix: 미들웨어 비활성화 및 main layout 서버사이드 렌더링 비활성화

--- src/app/(routes)/layout.tsx
+++ src/app/(routes)/layout.tsx
@@ -1,43 +1,18 @@
 import Footer from '@/components/layout/Footer';
 import GNB from '@/components/layout/GNB/GNB';
-import {
-  QueryClient,
-  dehydrate,
-  HydrationBoundary,
-} from '@tanstack/react-query';
-import { cookies } from 'next/headers';
 
-const getProfile = async () => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users/me`, {
-    credentials: 'include',
-    headers: {
-      'Content-Type': 'application/json',
-      Cookie: cookies().toString(),
-    },
-  });
-
-  return response.json();
-};
-
-export default async function Layout({
+export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const queryClient = new QueryClient();
-
-  await queryClient.prefetchQuery({
-    queryKey: ['me'],
-    queryFn: getProfile,
-  });
-
   return (
-    <HydrationBoundary state={dehydrate(queryClient)}>
+    <>
       <GNB />
       <main className='flex-1 w-full flex flex-col items-center'>
         {children}
       </main>
       <Footer />
-    </HydrationBoundary>
+    </>
   );
 }
