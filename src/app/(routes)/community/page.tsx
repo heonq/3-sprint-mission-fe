d30a4b1060659a4f946b3c9b333177ac92c8cf76
heonq <gkagjsrb@naver.com>
2025-02-11 15:39:44 +0900

    feat: community 페이지 서버사이드 렌더링 적용

--- src/app/(routes)/community/page.tsx
+++ src/app/(routes)/community/page.tsx
@@ -3,18 +3,42 @@ import BestArticleListClient from '@/components/community/bestArticle/bestArticl
 import NormalArticleListClient from '@/components/community/normalArticle/normalArticleListClient';
 import SearchSection from '@/components/community/searchSection/searchSection';
 import { GetArticleListParams } from '@/services/api/types/article.types';
+import { createSearchParams } from '@/utils/createSearchParams';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 
-export default function Page({
+const getArticles = async (searchParams: GetArticleListParams) => {
+  const params = createSearchParams(searchParams);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/articles?${params}`,
+  );
+  const data = await response.json();
+
+  return data;
+};
+
+export default async function Page({
   searchParams,
 }: {
   searchParams: GetArticleListParams;
 }) {
+  const queryClient = new QueryClient();
+  queryClient.prefetchQuery({
+    queryKey: ['articles', ...Object.values(searchParams)],
+    queryFn: async () => await getArticles(searchParams),
+  });
+
   return (
-    <div className='w-full md:w-full xl:w-[1200px] p-4 md:p-6 xl:p-0 xl:py-6 flex flex-col'>
-      <BestArticleListClient />
-      <ArticleListHeader />
-      <SearchSection />
-      <NormalArticleListClient searchParams={searchParams} />
-    </div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <div className='w-full md:w-full xl:w-[1200px] p-4 md:p-6 xl:p-0 xl:py-6 flex flex-col'>
+        <BestArticleListClient />
+        <ArticleListHeader />
+        <SearchSection />
+        <NormalArticleListClient searchParams={searchParams} />
+      </div>
+    </HydrationBoundary>
   );
 }
