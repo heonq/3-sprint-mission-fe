b572e007e06185f7ca4d106d4a35494e4c62fbb8
heonq <gkagjsrb@naver.com>
2025-02-12 16:47:55 +0900

    feat: 사용하지 않는 이미지 업로드 api 호출 함수 제거

--- src/services/api/image.ts
+++ src/services/api/image.ts
@@ -1,22 +1,5 @@
 import axiosInstance from '@/lib/axios/axiosInstance';
 
-export const uploadImage = async (file: File): Promise<string[]> => {
-  try {
-    const formData = new FormData();
-    formData.append('image', file);
-
-    const { data } = await axiosInstance.post('/upload/image', formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-    });
-    return data.imageUrls;
-  } catch (e) {
-    console.error('이미지 업로드 실패', e);
-    throw e;
-  }
-};
-
 export const getUploadUrl = async (filename: string) => {
   try {
     const { data } = await axiosInstance.get('/upload/url', {
