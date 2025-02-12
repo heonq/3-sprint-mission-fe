8cddc0f2c3a17e4da4a1b75f8029948631a38f55
heonq <gkagjsrb@naver.com>
2025-02-12 16:42:09 +0900

    feat: S3에 이미지 업로드하는 기능 구현

--- src/services/api/image.ts
+++ src/services/api/image.ts
@@ -16,3 +16,34 @@ export const uploadImage = async (file: File): Promise<string[]> => {
     throw e;
   }
 };
+
+export const getUploadUrl = async (filename: string) => {
+  try {
+    const { data } = await axiosInstance.get('/upload/url', {
+      params: { filename },
+    });
+    return data;
+  } catch (e) {
+    console.error('업로드 url 생성 실패', e);
+    throw e;
+  }
+};
+
+export const uploadImageOnS3 = async ({
+  file,
+  url,
+}: {
+  file: File;
+  url: string;
+}) => {
+  try {
+    await axiosInstance.put(url, file, {
+      headers: {
+        'Content-Type': file.type,
+      },
+    });
+  } catch (e) {
+    console.error('이미지 업로드 실패', e);
+    throw e;
+  }
+};
