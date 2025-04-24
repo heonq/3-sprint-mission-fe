370c5770281c39ed64d49f6e44bc2d1e4817a50c
heonq <gkagjsrb@naver.com>
2025-04-24 23:33:05 +0900

    feat: 상품 상세페이지 서버사이드 렌더링 적용

--- src/app/(routes)/items/[id]/page.tsx
+++ src/app/(routes)/items/[id]/page.tsx
@@ -1,13 +1,42 @@
-'use client';
-
 import ReturnButton from '@/components/common/returnButton/returnButton';
 import ProductInformationSection from '@/components/items/itemPage/productInformationSection';
 import { ParamsProps } from '@/lib/types/props.types';
 import CommentListClient from '@/components/common/comment/commentListClient';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
+import { cookies } from 'next/headers';
 
-export default function ProductPage({ params }: ParamsProps) {
+const getProductDetail = async (id: string) => {
+  const cookieStore = cookies();
+  const accessToken = cookieStore.get('accessToken')?.value;
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/products/${id}`,
+    {
+      headers: {
+        Cookie: `accessToken=${accessToken}`,
+      },
+      credentials: 'include',
+      next: { revalidate: 60 },
+    },
+  );
+  if (!response.ok) {
+    throw new Error('Failed to fetch data');
+  }
+  return response.json();
+};
+
+export default async function ProductPage({ params }: ParamsProps) {
+  const queryClient = new QueryClient();
+  queryClient.prefetchQuery({
+    queryKey: ['product', params.id],
+    queryFn: async () => await getProductDetail(params.id),
+  });
+
   return (
-    <>
+    <HydrationBoundary state={dehydrate(queryClient)}>
       <div className='w-full md:w-full xl:w-[1200px] p-4 md:p-6 xl:p-0 xl:py-6 flex flex-col'>
         <ProductInformationSection id={params.id} />
         <CommentListClient
@@ -16,6 +45,6 @@ export default function ProductPage({ params }: ParamsProps) {
         />
         <ReturnButton destination='/items' />
       </div>
-    </>
+    </HydrationBoundary>
   );
 }
